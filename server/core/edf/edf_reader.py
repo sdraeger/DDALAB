e16b99ee1d3afb337c1546abffb97080cc936579
Simon Dräger <sfdraeger@gmail.com>
2025-03-20 15:27:08 -0700

    Add dedicated edf reader class

--- server/core/edf/edf_reader.py
+++ server/core/edf/edf_reader.py
@@ -1,7 +1,8 @@
 """EDF file reader implementation."""
 
+from typing import Dict, List, Optional, Tuple
+
 import numpy as np
-from typing import Dict, List, Tuple, Optional
 from loguru import logger
 
 
