aa1ee9125b67b9540d6d64f49f216d749c307020
Simon Dräger <sfdraeger@gmail.com>
2025-03-27 15:26:01 -0700

    Remove old tests

--- ddalab-web/__tests__/mocks/theme-provider-mock.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-// Mock theme provider that doesn't use window.matchMedia
-export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  return <div data-testid="theme-provider-mock">{children}</div>;
-};
